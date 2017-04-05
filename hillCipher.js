2924c82fff1f77cd7408be77de6988b061313790
OrghoN <anoronyo@gmail.com>
2017-04-05 03:43:19 -0500

    filter and chunk text

--- hillCipher.js
+++ hillCipher.js
@@ -1,8 +1,14 @@
 const readFiles = require('read-multiple-files');
 const math = require('mathjs');
+const pad = require('array-pad')
 const modulo = 29;
 
 var text = "The quick brown fox jumps over the lazy dog.?!";
+var key = math.matrix([
+    [6, 24, 1],
+    [13, 16, 10],
+    [20, 17, 15]
+])
 
 // readFiles(['hello.txt', 'world.txt'], 'utf8', (err, contents) => {
 //   if (err) {
@@ -37,11 +43,21 @@ function createEncodingKey(n = 3, max = 1000) {
     return math.matrix(encodingKey);
 }
 
-function code(key,text){
-  // var block = math.size(key);
+function code(key, text) {
+    var block = math.size(key)._data[0];
 
-  text = text.match(/[A-Z .]/gi).join("");
-  return text;
+    //filter text for anything that can be coded and chunking it
+    text = text.toLowerCase().match(/[A-Z .]/gi).map(function(item, index, array) {
+        return index % block === 0 ? array.slice(index, index + block) : null;
+    }).filter(function(item) {
+        return item;
+    });
+
+    //padding the last chunk if required
+    text[text.length-1] = pad(text[text.length-1],block," ")
+
+
+    return text;
 }
 
-console.log(code(5,text));
+console.log(code(key, text));
