144c985b39c6748b1e3efa12b779bd137abe4ed0
OrghoN <anoronyo@gmail.com>
2017-04-04 23:47:11 -0500

    added math js dependency

--- hillCipher.js
+++ hillCipher.js
@@ -1,9 +1,16 @@
 const readFiles = require('read-multiple-files');
+const math = require('mathjs');
 
-readFiles(['hello.txt', 'world.txt'], 'utf8', (err, contents) => {
-  if (err) {
-    throw err;
-  }
+// readFiles(['hello.txt', 'world.txt'], 'utf8', (err, contents) => {
+//   if (err) {
+//     throw err;
+//   }
+//
+//   console.log(contents);
+// });
 
-  console.log(contents);
-});
+function createKey(n=3) {
+  
+}
+
+createKey();
