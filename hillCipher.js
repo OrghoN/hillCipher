cb77252bc6bc974fa00034a4069a82b4d5a98daa
OrghoN <anoronyo@gmail.com>
2017-04-05 02:58:41 -0500

    create encodingkey

--- hillCipher.js
+++ hillCipher.js
@@ -1,5 +1,6 @@
 const readFiles = require('read-multiple-files');
 const math = require('mathjs');
+const modulo = 29;
 
 // readFiles(['hello.txt', 'world.txt'], 'utf8', (err, contents) => {
 //   if (err) {
@@ -11,17 +12,27 @@ const math = require('mathjs');
 
 function createDecodingKey(encodingKey) {
     var det = math.det(encodingKey);
-    var detInv = math.xgcd(det, 26).toArray()[1];
+    var detInv = math.xgcd(det, modulo).toArray()[1];
 
     var decodingKey = math.chain(encodingKey).inv().map(function(val) {
-        return math.mod((detInv * math.round(val * det)), 26)
+        return math.mod((detInv * math.round(val * det)), modulo)
     }).done();
 
     return decodingKey;
 }
 
-function createEncodingKey(n = 3) {
-    return math.random([n,n], 1, 200);
+function createEncodingKey(n = 3, max = 1000) {
+    var det = 0;
+    var encodingKey
+    while (det === 0) {
+        encodingKey = math.random([n, n], 1, max);
+        encodingKey = math.map(encodingKey, function(val) {
+            return math.round(val);
+        });
+        det = math.det(encodingKey);
+    }
+
+    return math.matrix(encodingKey);
 }
 
 console.log(createEncodingKey());
