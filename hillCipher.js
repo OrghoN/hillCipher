74a36dcddcdbc98a1b42922ab73325b6158da181
OrghoN <anoronyo@gmail.com>
2017-04-05 02:47:54 -0500

    get decoding key from encoding key

--- hillCipher.js
+++ hillCipher.js
@@ -9,8 +9,19 @@ const math = require('mathjs');
 //   console.log(contents);
 // });
 
-function createKey(n=3) {
-  
+function createDecodingKey(encodingKey) {
+    var det = math.det(encodingKey);
+    var detInv = math.xgcd(det, 26).toArray()[1];
+
+    var decodingKey = math.chain(encodingKey).inv().map(function(val) {
+        return math.mod((detInv * math.round(val * det)), 26)
+    }).done();
+
+    return decodingKey;
 }
 
-createKey();
+function createEncodingKey(n = 3) {
+    return math.random([n,n], 1, 200);
+}
+
+console.log(createEncodingKey());
