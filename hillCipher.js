15412fb52481fe8cfba9636a4d8773210b38c05d
OrghoN <anoronyo@gmail.com>
2017-04-05 23:44:53 -0500

    made inverse using Cameron's code

--- hillCipher.js
+++ hillCipher.js
@@ -53,14 +53,61 @@ const decodingDict = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 //   console.log(contents);
 // });
 
+adjugate = function(matrix) {
+    var checkerBoard, column, columns, i, j, k, l, m, n, ref, ref1, ref2, ref3, ref4, results, results1, results2, results3, returnMatrix, row, rows, submatrix, wantedColumns, wantedRows;
+    ref = matrix.size(0), rows = ref[0], columns = ref[1];
+    returnMatrix = math.matrix([rows, columns]);
+    for (row = i = 0, ref1 = rows; 0 <= ref1 ? i < ref1 : i > ref1; row = 0 <= ref1 ? ++i : --i) {
+        for (column = j = 0, ref2 = columns; 0 <= ref2 ? j < ref2 : j > ref2; column = 0 <= ref2 ? ++j : --j) {
+            wantedRows = (function() {
+                results1 = [];
+                for (var l = 0; 0 <= row ? l < row : l > row; 0 <= row ? l++ : l--) {
+                    results1.push(l);
+                }
+                return results1;
+            }).apply(this).concat((function() {
+                results = [];
+                for (var k = ref3 = row + 1; ref3 <= rows ? k < rows : k > rows; ref3 <= rows ? k++ : k--) {
+                    results.push(k);
+                }
+                return results;
+            }).apply(this));
+            wantedColumns = (function() {
+                results3 = [];
+                for (var n = 0; 0 <= column ? n < column : n > column; 0 <= column ? n++ : n--) {
+                    results3.push(n);
+                }
+                return results3;
+            }).apply(this).concat((function() {
+                results2 = [];
+                for (var m = ref4 = column + 1; ref4 <= columns ? m < columns : m > columns; ref4 <= columns ? m++ : m--) {
+                    results2.push(m);
+                }
+                return results2;
+            }).apply(this));
+            submatrix = matrix.subset(math.index(wantedRows, wantedColumns));
+            returnMatrix.subset(math.index(row, column), -math.det(submatrix));
+        }
+    }
+    checkerBoard = function(value, index, matrix) {
+        var coordTotal;
+        coordTotal = index.reduce((function(a, b) {
+            return a + b;
+        }), 0);
+        return Math.pow(-1, coordTotal + 1) * value;
+    };
+    return math.transpose(returnMatrix.map(checkerBoard));
+};
+
 function createDecodingKey(encodingKey) {
     var det = math.det(encodingKey);
     var detInv = math.xgcd(det, modulo).toArray()[1];
 
     //creating mod inverse
-    var decodingKey = math.chain(encodingKey).inv().map(function(val) {
-        return math.mod((detInv * math.round(val * det)), modulo)
-    }).done();
+    var decodingKey = adjugate(encodingKey);
+    decodingKey = math.map(decodingKey, function(val) {
+        return math.mod((detInv * val), modulo);
+    });
 
     return decodingKey;
 }
@@ -110,4 +157,4 @@ key2 = createEncodingKey();
 
 console.log(code(key2, text));
 
-console.log(code(createDecodingKey(key2),code(key2, text)));
+console.log(code(createDecodingKey(key2), code(key2, text)));
